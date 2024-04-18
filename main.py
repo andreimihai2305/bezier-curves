3e3f97eaf2f601865307e87b23223033380f1fd0
Andrei Mihai <andreimihai2305is@gmail.com>
2024-04-18 09:25:13 -0400

    made points generation easily adjustable and their location random

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from settings import *
 from point import Point, draw_point, draw_line, draw_points
 from sys import exit
@@ -13,16 +14,12 @@ def main() -> None:
     pygame.display.set_caption("Bezier Curves")
     clock = pygame.time.Clock()
     
-    p1 = Point(100, 200, BLUE)
-    p2 = Point(200, 350, GREEN)
-    p3 = Point(400, 200, BLUE)
-    p4 = Point(800, 500, GREEN)
-    p5 = Point(500, 600, BLUE)
 
-    points: list[Point] = [p1, p2, p3, p4, p5]
-    curve_points1: list[Point] = compute_curve_points(p1, p2, p3)
-    curve_points2: list[Point] = compute_curve_points(p3, p4, p5)  
-    
+    points = create_origin_points(4)
+    control_points = create_control_points(3)
+    curve_points = compute_curve_points(points, control_points)
+
+    all_points = points + control_points
     selected = None
     
     # Main Game Loop
@@ -35,62 +32,102 @@ def main() -> None:
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    for num, point in enumerate(points):
+                    for num, point in enumerate(all_points):
                         if point.rect.collidepoint(event.pos):
                             selected = num
 
             elif event.type == pygame.MOUSEMOTION:
                 if selected != None:
-                    points[selected].x += event.rel[0]
-                    points[selected].y += event.rel[1]
-                    curve_points1 = compute_curve_points(p1, p2, p3)
-                    curve_points2 = compute_curve_points(p3, p4, p5)
-        
+                    all_points[selected].x += event.rel[0]
+                    all_points[selected].y += event.rel[1]
+                    curve_points = compute_curve_points(points, control_points) 
+
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     selected = None
 
 
         screen.fill(BLACK)
-       
-        draw_curve(screen, p1, p3, curve_points1)
-        draw_curve(screen, p3, p5, curve_points2)
+        
+        draw_curves(screen, points, curve_points)
         draw_points(screen, points)
-
+        draw_points(screen, control_points)
         pygame.display.update()
         clock.tick(FPS)
         
 
+def create_origin_points(num: int) -> list[Point]:
+    points = []
+    for i in range(num):
+        x = random.randrange(0, WIDTH)
+        y = random.randrange(0, HEIGHT)
+        points.append(Point(x, y, BLUE))
 
-def B(p1, p2, p3, t) -> Point:
+    return points
+                      
+
+
+def create_control_points(num: int) -> list[Point]:
+    control_points = []
+    for _ in range(num):
+        x = random.randrange(0, WIDTH)
+        y = random.randrange(0, HEIGHT)
+        control_points.append(Point(x, y, GREEN))
+                      
+    return control_points
+
+
+def B(start, control, end, t) -> Point:
     xp = 0
     yp = 0
-    x1, y1 = p1.x, p1.y
-    x2, y2 = p2.x, p2.y
-    x3, y3 = p3.x, p3.y
+    x0, y0 = start.x, start.y
+    x1, y1 = control.x, control.y
+    x2, y2 = end.x, end.y
 
-    xp = x2 + (1-t)**2 * (x1 - x2) + t**2 * (x3 - x2)
-    yp = y2 + (1-t)**2 * (y1 - y2) + t**2 * (y3 - y2)
+    xp = x1 + (1-t)**2 * (x0 - x1) + t**2 * (x2 - x1)
+    yp = y1 + (1-t)**2 * (y0 - y1) + t**2 * (y2 - y1)
     
     return Point(xp, yp, RED)
 
 
-def compute_curve_points(p1, p2, p3) -> list[Point]:
+def compute_curve_points(origin_points: list[Point], control_points: list[Point]) -> list[list[Point]]:
     curve_points = []
-    for i in range(20):
-        t = i / 20
-        pb = B(p1, p2, p3, t)
-        curve_points.append(pb)
+    if len(origin_points) != len(control_points) + 1:
+        print("Error: Points lists cannot be associated together. Origin points must be equal to control points + 1")
+        return [[]]
+
+    for i in range(len(origin_points) - 1):
+        current_points = []
+        for j in range(20):
+            t = j / 20
+                              
+            start = origin_points[i]
+            control = control_points[i]
+            end = origin_points[i + 1]
+            
+            pb = B(start, control, end, t)
+            current_points.append(pb)
+                              
+        curve_points.append(current_points)
     
     return curve_points
 
 
-def draw_curve(screen, start, end, points):
-    draw_line(screen, start, points[0], CYAN)
-    for i in range(1, len(points)):
-        draw_line(screen, points[i - 1], points[i], CYAN)
+def draw_curve(screen: pygame.Surface, start: Point, end: Point, curve_points: list[Point]) -> None:
 
-    draw_line(screen, points[-1], end, CYAN)
+    draw_line(screen, start, curve_points[0], RED)
+    for i in range(len(curve_points) - 1):
+        draw_line(screen, curve_points[i], curve_points[i + 1], CYAN)
+
+    draw_line(screen, curve_points[-1], end, GREEN)
+
+
+
+def draw_curves(screen: pygame.Surface, origin_points: list[Point], curve_points: list[Point]) -> None:
+    for i in range(len(origin_points) - 1):
+        draw_curve(screen, origin_points[i], origin_points[i + 1], curve_points[i])
+                            
+
 
 if __name__ == "__main__":
     main()
